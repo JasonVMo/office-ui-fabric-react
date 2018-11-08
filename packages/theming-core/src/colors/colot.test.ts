ec2707a5252a4dc60a6cfc109c69c26d60feb7fb
Tariq Amireh <tariq_amireh@hotmail.com>
2018-11-08 10:03:11 -0800

    Adding more tests

--- packages/theming-core/src/colors/colot.test.ts
+++ packages/theming-core/src/colors/colot.test.ts
@@ -1,7 +1,9 @@
 /**
  * These are temporary tests -- some are very likely to be changed
+ * And are by no means a full test suite
  */
 import { IRGB, IHSL, rgb2hsl, hsl2rgb } from './colors';
+import { relativeLuminance, calcContrastRatio, adjustForContrast } from './contrast';
 
 // IRGBs
 const white: IRGB = { r: 255, g: 255, b: 255 };
@@ -84,5 +86,44 @@ describe('conversion testing', () => {
     expect(result.g).toBeGreaterThanOrEqual(random.g - 1);
     expect(result.b).toBeLessThanOrEqual(random.b + 1);
     expect(result.b).toBeGreaterThanOrEqual(random.b - 1);
+  });
+});
+
+describe('contrast check', () => {
+  it('properly calculates relative luminance', () => {
+    let relLum: number = relativeLuminance(white.r, white.g, white.b);
+    expect(relLum).toEqual(1);
+    relLum = relativeLuminance(black.r, black.g, black.b);
+    expect(relLum).toEqual(0);
+    relLum = relativeLuminance(red.r, red.g, red.b);
+    expect(relLum).toBeCloseTo(0.213);
+    relLum = relativeLuminance(green.r, green.g, green.b);
+    expect(relLum).toBeCloseTo(0.715);
+    relLum = relativeLuminance(blue.r, blue.g, blue.b);
+    expect(relLum).toBeCloseTo(0.072);
+  });
+
+  it('properly calculates contrast ratio', () => {
+    let ratio: number = calcContrastRatio(white, black);
+    expect(ratio).toEqual(21);
+    ratio = calcContrastRatio(black, white);
+    expect(ratio).toEqual(21);
+    ratio = calcContrastRatio(red, green);
+    expect(ratio).toBeCloseTo(2.9, 1);
+    ratio = calcContrastRatio(red, blue);
+    expect(ratio).toBeCloseTo(2.1, 1);
+    ratio = calcContrastRatio(green, blue);
+    expect(ratio).toBeCloseTo(6.3, 1);
+  });
+
+  it('properly adjusts contrast', () => {
+    const newColor: IRGB = adjustForContrast(green, red, 4.2);
+    const ratio: number = calcContrastRatio(newColor, red);
+    // achieving a contrast ratio of 7 is impossible with red
+    // have to settle for 5
+    expect(ratio).toBeGreaterThanOrEqual(4);
+    const newColorHSL: IHSL = rgb2hsl(newColor);
+    expect(newColorHSL.h).toBeCloseTo(greenHSL.h);
+    expect(newColorHSL.s).toBeCloseTo(greenHSL.s);
   });
 });
