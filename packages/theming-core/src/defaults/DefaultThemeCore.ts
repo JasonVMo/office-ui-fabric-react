38fabbb83d73ae224848985bd81bc5144a1b8d00
Tariq Amireh <tariq_amireh@hotmail.com>
2018-11-19 16:42:19 -0800

    theming-core can function independently of all fabric packages (except set-version)

--- packages/theming-core/src/defaults/DefaultThemeCore.ts
+++ packages/theming-core/src/defaults/DefaultThemeCore.ts
@@ -1,13 +1,12 @@
 import { IThemeCore } from '../interfaces/index';
 import { createTypography } from '../utilities/index';
 import { DefaultPalette } from './DefaultColors';
-import { getLanguage } from '@uifabric/utilities';
 import { DefaultLayers } from './DefaultLayers';
 
 export function createThemeCore(): IThemeCore {
   return {
     palette: DefaultPalette,
-    typography: createTypography(getLanguage()),
+    typography: createTypography(null),
     layers: DefaultLayers,
     cache: {}
   };
